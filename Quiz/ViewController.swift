f2e45ff07494293e6254503f7ee5191c34f04272
Mamoudou DIALLO <82276477+Mah224Moud@users.noreply.github.com>
2024-12-13 08:42:58 +0100

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Mamoudou DIALLO on 13/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
