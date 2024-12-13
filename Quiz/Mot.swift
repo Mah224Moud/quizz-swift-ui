1f566e4308909dd60ce21db680a8c983d018da05
Mamoudou DIALLO <82276477+Mah224Moud@users.noreply.github.com>
2024-12-13 10:00:45 +0100

    finished the config page

---
+++ Quiz/Mot.swift
@@ -0,0 +1,8 @@
+//
+//  Mot.swift
+//  Quiz
+//
+//  Created by Mamoudou DIALLO on 13/12/2024.
+//
+
+import Foundation
--- Quiz/vcConfiguration2.swift
+++ Quiz/vcConfiguration2.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class vcConfiguration2: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
