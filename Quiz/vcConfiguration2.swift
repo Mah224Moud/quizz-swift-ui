032bf66329028135ae93debc7c53f078c8dd8386
Mamoudou DIALLO <82276477+Mah224Moud@users.noreply.github.com>
2024-12-13 10:42:24 +0100

    added the back for config

--- Quiz/vcConfiguration2.swift
+++ Quiz/vcConfiguration2.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class vcConfiguration2: UIViewController {
 
+    @IBOutlet weak var questionFraAng: UISwitch!
+    
+    @IBOutlet weak var questionAngFra: UISwitch!
+    
+    @IBOutlet weak var frWordInput: UITextField!
+    
+    @IBOutlet weak var enWordInput: UITextField!
+    
+    @IBOutlet weak var numberWords: UITextField!
+    var listeMots: [Mot] = []
+    var numberOfQuestions: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func add(_ sender: UIButton) {
+        let frWord = frWordInput.text?.isEmpty == false ? frWordInput.text : ""
+        let enWord = enWordInput.text?.isEmpty == false ? enWordInput.text : ""
+        
+        if frWord == ""{
+            print("Mot en français non renseigné")
+        }
+        if enWord == ""{
+            print("Mot en anglais non renseigné")
+        }
+        
+        if frWord != "" && enWord != "" {
+            listeMots.append(Mot(fr: frWord!, en: enWord!))
+            print("Ajouté: \(listeMots)")
+            numberOfQuestions += 1
+            frWordInput.text = ""
+            enWordInput.text = ""
+        }
+    }
+    
+    
+    @IBAction func play(_ sender: UIButton) {
+        print("Nombre de questions: \(numberOfQuestions) \nListe des mots: \(listeMots) \nQuestion en français: \(questionFraAng.isOn) \nQuestion en anglais: \(questionAngFra.isOn)")
+    }
+    
 }
 
